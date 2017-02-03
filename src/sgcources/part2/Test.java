ce15404490640d9ac92cf43369ff002987328e23
vlad2 <vlad.sereda.y@gmail.com>
2017-02-03 12:48:39 +0200

    Assignment 4 completed

--- src/sgcources/part2/Test.java
+++ src/sgcources/part2/Test.java
@@ -1,10 +1,12 @@
 package sgcources.part2;
 
+import sgcources.part2.program.ClosestQuakes;
 import sgcources.part2.program.EarthQuakeClient;
 
 public class Test {
     public static void main(String[] args) {
         EarthQuakeClient client = new EarthQuakeClient();
+        ClosestQuakes closestQuakes = new ClosestQuakes();
 
         announceTest("bigQuakes Test");
         client.bigQuakes();
@@ -22,6 +24,8 @@ public class Test {
         announceTest("quakesByPhrase3 Test");
         client.quakesByPhrase3();
 
+        announceTest("findClosestQuakes Test");
+        closestQuakes.findClosestQuakes();
     }
 
     private static void announceTest(String testName) {
--- src/sgcources/part2/program/ClosestQuakes.java
+++ src/sgcources/part2/program/ClosestQuakes.java
@@ -10,22 +10,25 @@ import java.util.*;
 
 public class ClosestQuakes {
     public ArrayList<QuakeEntry> getClosest(ArrayList<QuakeEntry> quakeData, Location current, int howMany) {
-        ArrayList<QuakeEntry> ret = new ArrayList<QuakeEntry>();
-        // TO DO
-
+        int size = quakeData.size() < howMany ? quakeData.size() : howMany;
+        ArrayList<QuakeEntry> ret = new ArrayList<>(size);
+        List<Entry> sortedList = sortByDistance(quakeData, current);
+        for (int i = 0; i < howMany; i++) {
+            ret.add(sortedList.get(i).getQuakeEntry());
+        }
         return ret;
     }
 
     public void findClosestQuakes() {
         EarthQuakeParser parser = new EarthQuakeParser();
         //String source = "data/nov20quakedata.atom";
-        String source = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.atom";
+        String source = "data/nov20quakedatasmall.atom";
         ArrayList<QuakeEntry> list  = parser.read(source);
         System.out.println("read data for "+list.size());
 
         Location jakarta  = new Location(-6.211,106.845);
 
-        ArrayList<QuakeEntry> close = getClosest(list,jakarta,10);
+        ArrayList<QuakeEntry> close = getClosest(list,jakarta,3);
         for(int k=0; k < close.size(); k++){
             QuakeEntry entry = close.get(k);
             double distanceInMeters = jakarta.distanceTo(entry.getLocation());
@@ -33,5 +36,36 @@ public class ClosestQuakes {
         }
         System.out.println("number found: "+close.size());
     }
-    
+
+
+    private List<Entry> sortByDistance(
+            ArrayList<QuakeEntry> quakeData,
+            Location current
+    ) {
+        List<Entry> sortedList = new ArrayList<>(quakeData.size());
+        for (QuakeEntry quakeEntry : quakeData) {
+            sortedList.add(new Entry(
+                    quakeEntry,
+                    quakeEntry.getLocation().distanceTo(current)
+            ));
+        }
+        Collections.sort(sortedList, (o1, o2) -> o1.getDistance().compareTo(o2.getDistance()));
+        return sortedList;
+    }
+
+    private static class Entry {
+        private QuakeEntry quakeEntry;
+        private Float distance;
+        Entry(QuakeEntry quakeEntry, Float distanceTo) {
+            this.quakeEntry = quakeEntry;
+            this.distance = distanceTo;
+        }
+        public QuakeEntry getQuakeEntry() {
+            return quakeEntry;
+        }
+        public Float getDistance() {
+            return distance;
+        }
+    }
+
 }
